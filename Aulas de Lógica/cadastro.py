761b3143486517544c0a2924d6cf55fd4bba90a1
dvinacioo <dviinacioo@gmail.com>
2025-04-28 11:02:18 -0300

    Cadastro básico em py

---
+++ Aulas de Lógica/cadastro.py
@@ -0,0 +1,25 @@
+#* CADASTRO BÁSICO FEITO NO CURSO DE LÓGICA *#
+
+print("DIGITE SEU NOME: ")
+nome = input()
+
+print("DIGITE SUA IDADE: ")
+idade = input()
+
+print("DIGITE SEU SEXO (M/F): ")
+sexo = input()
+
+print("DIGITE O NOME DA SUA RUA / N°: ")
+rua = input()
+
+print("DIGITE O NOME DO SEU BAIRRO: ")
+bairro = input()
+
+print("DIGITE SUA CIDADE: ")
+cidade = input()
+
+print("-"*25)
+print("SEU NOME É: ", nome)
+print( "SUA IDADE É: ", idade)
+print("SEU SEXO É: ", sexo)
+print("SUA ENDEREÇO É: RUA:", rua,"BAIRRO:",bairro, "CIDADE:", cidade)
