fae97026bba1bbd535b333c38a076490d43a9820
dvinacioo <dviinacioo@gmail.com>
2025-04-28 11:20:09 -0300

    Add código que verifica se o número é par ou impar

---
+++ Aulas de Lógica/par_impar.py
@@ -0,0 +1,9 @@
+# SISTEMA PARA SABER SE O NÚMERO DIGITADO É PAR ou IMPAR
+
+numero = int(input("DIGITE UM NÚMERO: "))
+
+if numero % 2 == 0:
+    print("O NÚMERO É PAR")
+
+else:
+    print("O NÚMERO É IMPAR")
