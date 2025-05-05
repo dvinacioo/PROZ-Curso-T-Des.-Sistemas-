c1398f27045e04116f2217c3469042eb7f5114d8
dvinacioo <dviinacioo@gmail.com>
2025-05-04 21:16:35 -0300

    ajuste no comentário

--- Aulas de Lógica/bonus_salario.py
+++ Aulas de Lógica/bonus_salario.py
@@ -1,6 +1,6 @@
 """
 desenvolver um programa que automatize o cálculo de bônus para os funcionários. O bônus é calculado com base no desempenho individual, avaliado por uma pontuação que varia de 0 a 100. O programa deve solicitar a pontuação do funcionário e, com base nisso, determinar o valor do bônus a ser concedido.
-O programa deve ser estruturado da seguinte maneira: Inicialmente, ele solicita ao usuário que insira a pontuação do funcionário. Dependendo dessa pontuação, o programa deve seguir um fluxo condicional. Se a pontuação for maior ou igual a 70, o funcionário recebe um bônus de 20% sobre o salário base. Se a pontuação for entre 50 e 69, o bônus é de 10%. Para pontuações abaixo de 50, não há bônus. O programa deve exibir o valor do bônus e uma mensagem personalizada sobre o desempenho.
+Inicialmente, ele solicita ao usuário que insira a pontuação do funcionário. Dependendo dessa pontuação, o programa deve seguir um fluxo condicional. Se a pontuação for maior ou igual a 70, o funcionário recebe um bônus de 20% sobre o salário base. Se a pontuação for entre 50 e 69, o bônus é de 10%. Para pontuações abaixo de 50, não há bônus. O programa deve exibir o valor do bônus e uma mensagem personalizada sobre o desempenho.
 - solicitar o saláario do funcionário
 - solicitar a pontuação do funcionário
 - se pontuação for Maior ou Igual a 70
