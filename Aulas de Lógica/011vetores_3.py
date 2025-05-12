51b3949fdb858be1c182b9a42f057178627d9819
dvinacioo <dviinacioo@gmail.com>
2025-05-12 11:21:08 -0300

    Add files via upload

---
+++ Aulas de Lógica/011vetores_3.py
@@ -0,0 +1,30 @@
+NOME = [0] * 8
+
+# Atribui strings (nomes) a cada posição da lista.
+NOME[0] = "Davi Inácio"
+NOME[1] = "Pedro Henrique"
+NOME[2] = "Pedro Almeida"
+NOME[3] = "Maria Eduarda"
+NOME[4] = "Enrico Wedson"
+NOME[5] = "Isabella Caroline"
+NOME[6] = "Nicolas Henrique"
+NOME[7] = "Wanderley"
+
+# Imprime cada nome da lista em uma linha
+print("Os Nomes dos Alunos são:")
+print(NOME[0])
+print(NOME[1])
+print(NOME[2])
+print(NOME[3])
+print(NOME[4])
+print(NOME[5])
+print(NOME[6])
+print(NOME[7])
+
+vetorDeValorLogico = [False] * 4
+vetorDeValorLogico[0] = True
+
+print(f"Na posição 1, o valor é.: {vetorDeValorLogico[0]}")
+print(f"Na posição 2, o valor é.: {vetorDeValorLogico[1]}")
+print(f"Na posição 3, o valor é.: {vetorDeValorLogico[2]}")
+print(f"Na posição 4, o valor é.: {vetorDeValorLogico[3]}")
