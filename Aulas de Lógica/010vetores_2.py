7dc695c83585c5367ad67793337caf9a3dda25e5
dvinacioo <dviinacioo@gmail.com>
2025-05-12 11:20:57 -0300

    Add files via upload

---
+++ Aulas de Lógica/010vetores_2.py
@@ -0,0 +1,8 @@
+#EXEMPLO 1 DE VETORES
+# Em python, não precisamos declarar o tipo e tamanho fixo
+# do vetor
+
+#Cria uma lista com 4 elementos, inicializados com 0
+vetorDeCaracter = [None] * 4 # Vetor que vai do índice 0 até 3 [0, 1, 2, 3]
+vetorDeCaracter[0] = "Davi"  # A posição 0 recebe valor "Davi"
+print(f"Na posição , o valor é {vetorDeCaracter[0]}")
