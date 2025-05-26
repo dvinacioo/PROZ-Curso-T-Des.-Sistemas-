5b8e46a5f3858463dd55b80478ca9ecf511e9c0b
dvinacioo <dviinacioo@gmail.com>
2025-05-26 10:51:59 -0300

    Add files via upload

---
+++ Aulas de Lógica/025_listas.py
@@ -0,0 +1,13 @@
+estacoes = ('primaver',  'verão', 'outono', 'inverno')
+alunos = ['Thiago', 'Pedro', 'Bianca', 'Jessica', 'Bruno']
+
+alunos.insert(1, 'Davi') # Insere "Davi" no índice 1
+print(alunos)
+
+
+alunos.sort() # Sorteia a Lista dos alunos em ordem aleatória
+print(alunos)
+
+
+alunos.pop(0) # Remove o índice "0" da lista
+print(alunos)
