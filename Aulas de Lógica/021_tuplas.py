9532eee0948d9af367f12ee741bbd12036622488
dvinacioo <dviinacioo@gmail.com>
2025-05-26 10:50:59 -0300

    Add files via upload

---
+++ Aulas de Lógica/021_tuplas.py
@@ -0,0 +1,9 @@
+# Tupla é uma estrutura de dados imutável, ou seja, após ser criada, seus elementos não podem ser alterados. 
+# Elas podem conter diferentes tipos de dados e são definidas utilizando parênteses. 
+# Tuplas são úteis quando você deseja garantir que os dados não sejam modificados acidentalmente.
+
+# Exemplo de tupla:
+
+estacoes = ('primavera', 'verão', 'outuno', 'inverno')
+print(estacoes)
+print(estacoes[3])
