4061dc6d9a54a85cf7db6bae96f8424f24bbac5b
dvinacioo <dviinacioo@gmail.com>
2025-05-26 10:50:40 -0300

    Add files via upload

---
+++ Aulas de Lógica/Trabalho Lógica/09.py
@@ -0,0 +1,9 @@
+# VERIFICA IDADE PARA DIRIGIR
+
+idade = int(input("Qual é a sua idade? "))
+
+if idade >= 18:
+    print("Você está apto para dirigir.")
+
+else:
+    print("Você não está apto para dirigir.")
