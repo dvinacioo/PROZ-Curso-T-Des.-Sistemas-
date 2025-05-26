92eac3d6ae497d2e76816e99a49aa5fb7831f8cd
dvinacioo <dviinacioo@gmail.com>
2025-05-26 10:51:29 -0300

    Add files via upload

---
+++ Aulas de Lógica/023_listas.py
@@ -0,0 +1,13 @@
+# Lista é uma estrutura de dados mutável, ou seja, seus elementos podem ser alterados após a criação.
+# Listas são definidas usando colchetes e podem armazenar diferentes tipos de dados.
+# São muito utilizadas em Python por sua flexibilidade e facilidade de uso.
+# Exemplo de lista:
+
+alunos = ['Davi', 'Pedro', 'Enrico', "Isabella"]
+
+print("Terceiro aluno: ",alunos[2])
+print("Quarto aluno: ",alunos[3])
+print(" ")
+
+for alunos in alunos:
+    print(alunos)
