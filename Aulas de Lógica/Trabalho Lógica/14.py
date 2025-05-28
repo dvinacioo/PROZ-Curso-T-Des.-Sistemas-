eb96426d13d23862c91d19020cc9b20b66efc20e
dvinacioo <dviinacioo@gmail.com>
2025-05-28 11:00:06 -0300

    Add files via upload

---
+++ Aulas de Lógica/Trabalho Lógica/14.py
@@ -0,0 +1,9 @@
+vogais=["a"," e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+x = input("Digite uma letra: ")
+
+if x in vogais:
+    print("Você digitou uma vogal.")
+
+else:
+    print("Você digitou um consoante")
