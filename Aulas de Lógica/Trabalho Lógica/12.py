1344a1b3108d824e642a0845ad9b7f7b9c64d1e5
dvinacioo <dviinacioo@gmail.com>
2025-05-28 10:59:30 -0300

    Add files via upload

---
+++ Aulas de Lógica/Trabalho Lógica/12.py
@@ -0,0 +1,11 @@
+# Aprovação em disciplina
+""" Solicite a nota do aluno em uma disciplina e
+determine se ele foi aprovado (nota maior ou igual a 7)"""
+
+nota= float(input("Digite sua nota: "))
+
+if nota >= 7:
+    print("Você foi aprovado.")
+
+else:
+    print("Você foi reprovado.")
