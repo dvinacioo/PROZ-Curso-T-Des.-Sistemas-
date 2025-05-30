2c5e8798a5f73305225af72a043670f3f3095998
dvinacioo <dviinacioo@gmail.com>
2025-05-30 11:00:51 -0300

    Add files via upload

---
+++ Aulas de Lógica/31_AreaCirculo.py
@@ -0,0 +1,6 @@
+# Programa que calcula a area de um circulo
+
+raio = float(input("Digite o Valor do Raio do Circulo: ")) # Solcita o valor do Raio do Circulo
+pi = 3.14 # Valor de PI
+area = pi * raio **2
+print(f"A Area do seu Circulo é: {area:.2f}")
