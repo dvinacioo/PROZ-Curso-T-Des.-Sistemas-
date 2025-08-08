709d0772f4a4b6df63dd839dd54e264bd4a8a976
dvinacioo <dviinacioo@gmail.com>
2025-08-08 11:06:08 -0300

    Add Pasta do Módulo ( Linguagem de Programação )

---
+++ LINGUAGEM DE PROGRAMAÇÃO/AULA 1/aula1.js
@@ -0,0 +1,4 @@
+function mudarMensagem() {
+    const men = document.getElementById("mensagem");
+    men.innerHTML = "Você acabou de GANHAR 10% de descontos em todos os produtos da loja! <br> Aproveite!"
+}
